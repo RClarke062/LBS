50091bbeba8d75c481cbbd76b86f246d318268b0
RClarke062 <robertjclarke@sympatico.ca>
2018-04-17 21:38:57 -0400

    Initial commit

---
+++ app/src/main/java/com/robboapps/lbs/MapsActivity.java
@@ -0,0 +1,92 @@
+package com.robboapps.lbs;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng ottawa = new LatLng(45.4215, -75.6972);
+        mMap.addMarker(new MarkerOptions().position(ottawa).title("Ottawa"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ottawa));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener(){
+            @Override
+            public void onMapClick(LatLng point){
+                Geocoder geoCoder = new Geocoder(
+                        getBaseContext(), Locale.getDefault());
+                try {
+                    List<Address> addresses = geoCoder.getFromLocation(
+                            point.latitude, point.longitude, 1);
+                    String add = "";
+                    if (addresses.size() > 0) {
+                        for (int i = 0; i < addresses.get(0).getMaxAddressLineIndex();
+                             i++)
+                            add += addresses.get(0).getAddressLine(i) + "\n";
+                    }
+                    Toast.makeText(getBaseContext(),
+                            add, Toast.LENGTH_SHORT).show();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                }
+
+        });
+    }
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        switch (keyCode)
+        {
+            case KeyEvent.KEYCODE_3:
+                mMap.animateCamera(CameraUpdateFactory.zoomIn());
+                break;
+            case KeyEvent.KEYCODE_1:
+                mMap.animateCamera(CameraUpdateFactory.zoomOut());
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
